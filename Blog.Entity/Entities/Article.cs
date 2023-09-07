b3116a9eee2c816f3e05d1f2372ba92dadd559c9
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-09-07 11:40:09 +0300

    .

---
+++ Blog.Entity/Entities/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Core.Entities;
+
+namespace Blog.Entity.Entities
+{
+    public class Article:EntityBase,IEntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewCount { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category Category{ get; set; }
+
+        public Guid ImageId { get; set; }
+        public Image Image { get; set; }
+
+    }
+}
