05cffe5e49a0ca8091dad690c7363907bb77b9d2
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-05-05 16:30:22 +0300

    blog demo + AutoMapper

--- Blog.Service/Services/Concrete/ArticleService.cs
+++ Blog.Service/Services/Concrete/ArticleService.cs
@@ -1,4 +1,6 @@
-﻿using Blog.Data.UnitOfWork;
+﻿using AutoMapper;
+using Blog.Data.UnitOfWork;
+using Blog.Entity.DTOs.Articles;
 using Blog.Entity.Entities;
 using Blog.Service.Services.Abstractions;
 using System;
@@ -12,14 +14,19 @@ namespace Blog.Service.Services.Concrete
     public class ArticleService : IArticleService
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
 
-        public ArticleService(IUnitOfWork unitOfWork)
+        public ArticleService(IUnitOfWork unitOfWork,IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
-        public async Task<List<Article>> GetAllArticleAsync()
+        public async Task<List<ArticleDto>> GetAllArticleAsync()
         {
-            return await unitOfWork.GetRepository<Article>().GetAllAsync();
+         var articles= await unitOfWork.GetRepository<Article>().GetAllAsync();
+
+            var map = mapper.Map<List<ArticleDto>>(articles);
+            return map;
         }
     }
 }
